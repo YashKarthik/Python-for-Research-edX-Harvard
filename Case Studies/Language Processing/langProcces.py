009a5a775e68c7323e2ddfc715467aaa7a4c048e
Yash <52369876+Yash10bloodstorm@users.noreply.github.com>
2020-04-06 11:25:01 +0530

    Language Processing Done

--- Case Studies/Language Processing/langProcces.py
+++ Case Studies/Language Processing/langProcces.py
@@ -1,6 +1,10 @@
 from collections import Counter
+import os
+import pandas as pd
+import matplotlib.pyplot as plt 
 
 
+book_dir = 'Language Processing/Books'
 test_str = 'This is just a test string to keep things manageable...'
 
 def count_words(text):
@@ -43,8 +47,6 @@ def read_book(title_path):
         text = text.replace('\n', '').replace('\r', '')
         return text
 
-read_book('/Users/Yash/Documents/Code/Course-Python-for-research/Case Studies/Language Processing/Books_EngFr/English/shakespeare/Romeo and Juliet.txt')
-
 # Computing word frequency statistics
 
 def word_stats(word_counts):
@@ -53,3 +55,37 @@ def word_stats(word_counts):
     counts = word_counts.values()
     return (num_unique, counts)
 
+stats = pd.DataFrame(columns=('Language', 'Author', 'Title', 'Length', 'Unique'))
+title_num = 1
+
+
+
+for language in os.listdir(book_dir):
+    for author in os.listdir(book_dir + '/' + language):
+        for title in os.listdir(book_dir + '/' + language + '/' + author):
+            inputfile = book_dir + '/' + language + '/' + author + '/' + title
+            print(inputfile)
+            text = read_book(inputfile)
+            num_unique = word_stats(count_words(text))
+            stats.loc[title_num]= language,author.capitalize(),title.replace(".txt",""),sum(word_stats.counts),num_unique
+            title_num += 1
+
+# Plot
+
+plt.figure(figsize = (10, 10))
+subset = stats[stats.language == "English"]
+plt.loglog(subset.length,  subset.unique, "o", label = "English", color = "crimson");
+
+subset = stats[stats.language == "French"]
+plt.loglog(subset.length,  subset.unique, "o", label = "French", color = "forestgreen");
+
+subset = stats[stats.language == "German"]
+plt.loglog(subset.length,  subset.unique, "o", label = "German", color = "orange");
+
+subset = stats[stats.language == "Portuguese"]
+plt.loglog(subset.length,  subset.unique, "o", label = "Portuguese", color = "blueviolet");
+
+plt.legend()
+plt.xlabel("Book length")
+plt.ylabel("Number of unique words")
+plt.savefig("lang_plot.pdf")
